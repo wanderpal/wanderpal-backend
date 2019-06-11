2b26b25f8767e229da6b2e9c86d57def16403c93
Hliana <liana.niehrah@gmail.com>
2019-06-10 21:18:05 -0700

    changing to es5

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 'use strict';
 
-import mongoose from 'mongoose';
-import * as app from './src/app';
-
 require('dotenv').config();
 const mongodb = process.env.MONGODB_URI || 'Mongodb';
 
+const mongoose = require('mongoose');
 const options = {
   useNewUrlParser: true,
   useCreateIndex: true
 };
 mongoose.connect(mongodb, options);
 
-app.start(process.env.PORT || 5000);
+require('./src/app.js').start(process.env.PORT || 5000);
