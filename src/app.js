81f2c90b6896adcb58dc813032ceb478f40da3c4
Rick Bell <richard.bellamy123@gmail.com>
2019-06-11 21:34:19 -0700

    for the night 2

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const errorHandler = require('./middleware/error.js');
+const errorHandler = require('./middleware/500.js');
 const notFound = require('./middleware/404.js');
 const authRouter = require('./auth/router.js');
 
