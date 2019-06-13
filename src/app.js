38a8de2f279987c4541cb5531e258bd4fd8dac52
Rick Bell <richard.bellamy123@gmail.com>
2019-06-12 20:32:42 -0700

    logging in

--- src/app.js
+++ src/app.js
@@ -4,13 +4,16 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const errorHandler = require('./middleware/500.js');
-const notFound = require('./middleware/404.js');
+// const errorHandler = require('./middleware/500.js');
+// const notFound = require('./middleware/404.js');
 const authRouter = require('./auth/router.js');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cors());
 app.use(morgan('dev'));
+app.use(authRouter);
 
 let isRunning = false;
 
--- src/auth/router.js
+++ src/auth/router.js
@@ -6,10 +6,16 @@ const authRouter = express.Router();
 const User = require('../models/users-model.js');
 const auth = require('./middleware.js');
 const oauth = require('./oauth/google.js');
-
+//console.log(authRouter);
+// authRouter.get('/hello', (req, res, next) => {
+//     res.status(200).send('hello');
+// });
 authRouter.post('/signup', (req, res, next) => {
-  let user = new User(req.body);
-  user.save()
+    console.log('req.body', '========================================');
+    let user = new User(req.body);
+    console.log(req.body);
+    console.log(user);
+    user.save()
     .then( (user) => {
       req.token = user.generateToken();
       req.user = user;
